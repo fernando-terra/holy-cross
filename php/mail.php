f499a5c47a775aa7d27fdaeb0f86d318c2cddde6
Fernando Terra <fernando.terras@outlook.com>
2018-08-06 22:03:50 -0300

    Fix send mail method

--- php/mail.php
+++ php/mail.php
@@ -3,5 +3,10 @@
     $mail_subject = 'Pedido de oração';
     $mail_body = $_POST['body'];
 
-    mail($mail_to, $mail_subject, $mail_body);
+    $header = "From: Pedido de Oração <pedidodeoracao@sagradacruz.com.br>\r\n"; 
+    $header.= "MIME-Version: 1.0\r\n"; 
+    $header.= "Content-Transfer-Encoding: 7bit\r\n";
+    $header.= sprintf("Content-Type: %s; charset=\"%s\"","text/html","utf-8")."\n\n";
+    
+    mail($mail_to, $mail_subject, wordwrap($mail_body, 70), $header);
 ?>
